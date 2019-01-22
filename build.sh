4566acff7ddbf475f17bf27fb511a228274e5687
dramforever <dramforever@live.com>
2019-01-22 12:58:34 +0800

    Use build/ for build output

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 
 set -eo pipefail
 
+mkdir -p build
+
 set -x
-nix-build "$@" -A nixBoot -o nix-stage-1
+nix-build "$@" -A nixBoot -o build/nix-stage-1
 set +x
 
 mkdir -p "/tmp/nix/etc/nix"
@@ -18,11 +20,11 @@ END
 fi
 
 set -x
-nix-stage-1/bin/nix-build "$@" -A nixBoot -o nix-stage-2
-nix-stage-1/bin/nix-build "$@" -A nixInstaller -o link-nix-installer.tar.xz
-[ -e nix-installer.tar.xz ] && rm nix-installer.tar.xz
-cat link-nix-installer.tar.xz > nix-installer.tar.xz
-nix-stage-1/bin/nix-build "$@" -A nixWithStdenvInstaller -o link-nix-stdenv-installer.tar.xz
-[ -e nix-stdenv-installer.tar.xz ] && rm nix-stdenv-installer.tar.xz
-cat link-nix-stdenv-installer.tar.xz > nix-stdenv-installer.tar.xz
+build/nix-stage-1/bin/nix-build "$@" -A nixBoot -o build/nix-stage-2
+build/nix-stage-1/bin/nix-build "$@" -A nixInstaller -o build/link-nix-installer.tar.xz
+[ -e build/nix-installer.tar.xz ] && rm build/nix-installer.tar.xz
+cat build/link-nix-installer.tar.xz > build/nix-installer.tar.xz
+build/nix-stage-1/bin/nix-build "$@" -A nixWithStdenvInstaller -o build/link-nix-stdenv-installer.tar.xz
+[ -e build/nix-stdenv-installer.tar.xz ] && rm build/nix-stdenv-installer.tar.xz
+cat build/link-nix-stdenv-installer.tar.xz > build/nix-stdenv-installer.tar.xz
 set +x
